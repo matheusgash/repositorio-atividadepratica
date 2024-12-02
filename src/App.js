8e4698ae1e4e0c997b0531a5994aac1033b0c5d8
matheusgash <matheusxdd1@hotmail.com>
2024-12-02 13:53:51 -0300

    menu de categorias pagina principal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
 import Login from "./pages/login";
+import PaginaInicialCategorias from "./pages/categoria/PaginaInicialCategoria"; 
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Login />
+      <PaginaInicialCategorias />
+      
     </div>
   );
 }
